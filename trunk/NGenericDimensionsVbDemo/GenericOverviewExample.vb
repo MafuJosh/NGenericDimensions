514b79f8879166a0d4d4aa412684189cbf9ae035
MafuJosh_cp <MafuJosh@users.noreply.github.com>
2011-09-18 02:59:20 +0000

    This project will be for example/demo VB.NET use of the NGenericDimensions library.

---
+++ trunk/NGenericDimensionsVbDemo/GenericOverviewExample.vb
@@ -0,0 +1,16 @@
+﻿Imports NGenericDimensions
+Imports NGenericDimensions.Lengths.MetricSI
+Imports NGenericDimensions.Extensions
+Imports NGenericDimensions.Extensions.Numbers
+Public Class GenericOverviewExample
+    Public Shared Sub Demo()
+        Dim myspeed = 10.miles.per.hour
+        Dim yourspeed = 20.kilometres / 2.minutes
+        Dim mylength1 As New Length(Of Metres, Double)(100.0)
+        Dim mylength2 As New Length(Of Metres, Double)(200.0)
+        Dim mylenght3 = mylength1 + mylength2
+        Dim myarea = mylength1 * mylength2
+        Dim myarea2 As Area(Of Metres, Double) = myarea
+        Dim myduration As New Duration(Of Durations.Minutes, Integer)(100)
+    End Sub
+End Class
---
+++ trunk/NGenericDimensionsVbDemo/Module1.vb
@@ -0,0 +1,11 @@
+﻿Module Module1
+
+    Sub Main()
+
+    End Sub
+
+End Module
+
+Public Class ExampleUsingNumericExtensions
+
+End Class
