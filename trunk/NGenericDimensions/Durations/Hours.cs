571bde8cb82ea54e67cc83ac1057a60a9190b9b7
MafuJosh_cp <MafuJosh@users.noreply.github.com>
2011-09-18 02:57:15 +0000

    start main project

---
+++ trunk/NGenericDimensions/Durations/Hours.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.ComponentModel;
+
+namespace NGenericDimensions.Durations
+{
+
+    public class Hours : StandardDurationUnitOfMeasure
+    {
+
+        protected override double GetMultiplier(bool stayWithinFamily)
+        {
+            return 36000000000L;
+        }
+
+    }
+
+}
+
+namespace NGenericDimensions.Extensions
+{
+
+    public static class HoursExtensionMethods
+    {
+
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        public static T HoursValue<T>(this Duration<Durations.Hours, T> duration) where T : struct, IComparable, IConvertible
+        {
+            return duration.DurationValue;
+        }
+
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        public static Nullable<T> HoursValue<T>(this Nullable<Duration<Durations.Hours, T>> duration) where T : struct, IComparable, IConvertible
+        {
+            if (duration.HasValue)
+            {
+                return duration.Value.DurationValue;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        [EditorBrowsable(EditorBrowsableState.Advanced)]
+        public static Speed<TUnitOfMeasure, Durations.Hours, TDataType> hour<TUnitOfMeasure, TDataType>(this LengthPerExtension<TUnitOfMeasure, TDataType> length)
+            where TUnitOfMeasure : Lengths.Length1DUnitOfMeasure
+            where TDataType : struct, IComparable, IConvertible
+        {
+            return length.Length.LengthValue;
+        }
+
+    }
+
+}
+
+namespace NGenericDimensions.Extensions.Numbers
+{
+
+    public static class HoursNumberExtensionMethods
+    {
+
+        [EditorBrowsable(EditorBrowsableState.Advanced)]
+        public static Duration<Durations.Hours, T> hours<T>(this T duration) where T : struct, IComparable, IConvertible
+        {
+            return duration;
+        }
+
+    }
+
+}
---
+++ trunk/NGenericDimensions/Lengths/MetricSI/Kilometres.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.ComponentModel;
+using NGenericDimensions.MetricPrefix;
+
+namespace NGenericDimensions.Lengths.MetricSI
+{
+
+    public class Kilometres : Metres<Kilo>
+    {
+
+    }
+
+}
+
+namespace NGenericDimensions.Extensions
+{
+
+    public static class KilometresExtensionMethods
+    {
+
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        public static T KilometresValue<T>(this Length<Lengths.MetricSI.Kilometres, T> length) where T : struct, IComparable, IConvertible
+        {
+            return length.LengthValue;
+        }
+
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        public static Nullable<T> KilometresValue<T>(this Nullable<Length<Lengths.MetricSI.Kilometres, T>> length) where T : struct, IComparable, IConvertible
+        {
+            if (length.HasValue)
+            {
+                return length.Value.LengthValue;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        public static T SquareKilometresValue<T>(this Area<Lengths.MetricSI.Kilometres, T> area) where T : struct, IComparable, IConvertible
+        {
+            return area.AreaValue;
+        }
+
+        [EditorBrowsable(EditorBrowsableState.Always)]
+        public static Nullable<T> SquareKilometresValue<T>(this Nullable<Area<Lengths.MetricSI.Kilometres, T>> area) where T : struct, IComparable, IConvertible
+        {
+            if (area.HasValue)
+            {
+                return area.Value.AreaValue;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+    }
+
+}
+
+namespace NGenericDimensions.Extensions.Numbers
+{
+
+    public static class KilometresNumberExtensionMethods
+    {
+
+        [EditorBrowsable(EditorBrowsableState.Advanced)]
+        public static Length<Lengths.MetricSI.Kilometres, T> kilometres<T>(this T length) where T : struct, IComparable, IConvertible
+        {
+            return length;
+        }
+
+        [EditorBrowsable(EditorBrowsableState.Advanced)]
+        public static Area<Lengths.MetricSI.Kilometres, T> kilometres<T>(this LengthSquareExtension<T> area) where T : struct, IComparable, IConvertible
+        {
+            return area.Area;
+        }
+
+    }
+
+}
