6317ab84272c57a6d2e7fd5256efca609758590f
MafuJosh_cp <SND\MafuJosh_cp>
2011-09-18 03:21:27 +0000

    fixed references, cleaned up code

--- trunk/NGenericDimensionsVbDemo/ExampleUsingNumericExtensions.vb
+++
@@ -1,8 +0,0 @@
-﻿Imports NGenericDimensions.Extensions
-Imports NGenericDimensions.Extensions.Numbers
-Public Class ExampleUsingNumericExtensions
-    Public Shared Sub Demo()
-        Dim myspeed = 10.miles.per.hour
-        Dim yourspeed = 20.kilometres / 2.minutes
-    End Sub
-End Class
--- trunk/NGenericDimensionsVbDemo/GenericOverviewExample.vb
+++ trunk/NGenericDimensionsVbDemo/GenericOverviewExample.vb
@@ -8,7 +8,8 @@ Public Class GenericOverviewExample
         Dim yourspeed = 20.kilometres / 2.minutes
         Dim mylength1 As New Length(Of Metres, Double)(100.0)
         Dim mylength2 As New Length(Of Metres, Double)(200.0)
-        Dim mylenght3 = mylength1 + mylength2
+        Dim mylength3 As Length(Of Metres, Double) = 300.0
+        Dim mylenght4 = mylength1 + mylength2
         Dim myarea = mylength1 * mylength2
         Dim myarea2 As Area(Of Metres, Double) = myarea
         Dim myduration As New Duration(Of Durations.Minutes, Integer)(100)
