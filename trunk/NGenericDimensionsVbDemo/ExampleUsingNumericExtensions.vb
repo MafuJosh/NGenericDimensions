25516f98155df741e259fa16d63e6a235d78659b
MafuJosh_cp <SND\MafuJosh_cp>
2011-09-18 02:59:20 +0000

    This project will be for example/demo VB.NET use of the NGenericDimensions library.

---
+++ trunk/NGenericDimensionsVbDemo/ExampleUsingNumericExtensions.vb
@@ -0,0 +1,8 @@
+﻿Imports NGenericDimensions.Extensions
+Imports NGenericDimensions.Extensions.Numbers
+Public Class ExampleUsingNumericExtensions
+    Public Shared Sub Demo()
+        Dim myspeed = 10.miles.per.hour
+        Dim yourspeed = 20.kilometres / 2.minutes
+    End Sub
+End Class
