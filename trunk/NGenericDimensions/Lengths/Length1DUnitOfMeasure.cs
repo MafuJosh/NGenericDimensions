ab7d9cc004b59807d44c6885aa42440bcf59a215
MafuJosh_cp <SND\MafuJosh_cp>
2011-09-18 02:57:15 +0000

    start main project

---
+++ trunk/NGenericDimensions/Lengths/Length1DUnitOfMeasure.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+namespace NGenericDimensions.Lengths
+{
+
+    public abstract class Length1DUnitOfMeasure : LengthUnitOfMeasure
+    {
+
+    }
+
+}
